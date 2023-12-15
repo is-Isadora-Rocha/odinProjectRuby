1f56cc1d142a857434342536bfacb74575fa80b6
is-Isadora-Rocha <is.isadora.rocha@gmail.com>
2023-12-15 15:36:21 -0300

    finalizando matriz

--- curso_ruby/matrizes.rb
+++ curso_ruby/matrizes.rb
@@ -92,6 +92,23 @@ puts "#{z}"
 
 
 # MÉTODOS BÁSICOS 
+# Chamar o método #methods em um array também produzirá uma longa lista de métodos disponíveis
 
+num_array.methods #=> longa lista de métodos 
+
+#métodos de array 
+[].empty?               #=> true
+[[]].empty?             #=> false
+[1, 2].empty?           #=> false
+
+[1, 2, 3].length        #=> 3
+
+[1, 2, 3].reverse       #=> [3, 2, 1]
+
+[1, 2, 3].include?(3)   #=> true
+[1, 2, 3].include?("3") #=> false
+
+[1, 2, 3].join          #=> "123"
+[1, 2, 3].join("-")     #=> "1-2-3"
 
 
