207d9cf9c424c129b3a65592c67fae8537dd84b4
is-Isadora-Rocha <is.isadora.rocha@gmail.com>
2023-12-15 15:44:24 -0300

    hash

---
+++ curso_ruby/hashes.rb
@@ -0,0 +1,12 @@
+# HASH 
+# primo sobrecarregado do array 
+
+# Matriz != Hash 
+# Matriz => uma máquina de venda automática de comida, no qual
+# a ordem não altera, o 12 sempre virá antes do 13 e depois do 11.
+
+# Hash = é um menu cardápio no qual você pode encontrar o que procura
+# desde que use o nome correto. E é não ordenado. 
+
+
+# CRIANDO UM HASH 
